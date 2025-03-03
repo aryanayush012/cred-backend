5c638efaa7551e3e17f102430818974f350539e1
Ayush Aryan <aryanayush012@gmail.com>
2025-03-03 20:12:20 +0530

    fixed mongo db issue

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const mongoURI =
-  "mongodb+srv://aryanayush012:qMqgDewQficUyoSM@cluster0.dtw0vgm.mongodb.net/card";
+  // "mongodb+srv://aryanayush012:qMqgDewQficUyoSM@cluster0.dtw0vgm.mongodb.net/card";
+  "mongodb+srv://aryanayush012:mwGK839te2e30Tad@cluster0.cjjz3.mongodb.net/"
 
 const connectToMongo = async () => {
   await mongoose.connect(mongoURI);
