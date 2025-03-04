f7e131981a0a08d6eb14ac9978e1b73f33b7caa1
Ayush Aryan <aryanayush012@gmail.com>
2025-03-04 22:45:57 +0530

    moved index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const connectToMongo = require("./db");
+const connectToMongo = require("../db");
 const express = require("express");
 var cors = require("cors");
 const path = require("path");
@@ -15,8 +15,8 @@ app.use(cors({
 app.use(express.json());
 
 // Available Routes
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/notes", require("./routes/notes"));
+app.use("/api/auth", require("../routes/auth"));
+app.use("/api/notes", require("../routes/notes"));
 
 // app.use(express.static(path.join(__dirname, "../build")));
 // app.get("*", (req, res) => {
