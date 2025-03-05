a5f1859b1eac5017cd03ad7923c794b0ff134bb3
Ayush Aryan <aryanayush012@gmail.com>
2025-03-05 20:51:51 +0530

    regular patch update

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(cors({
 app.use(express.json());
 
 // Available Routes
-app.use("/api/auth", require("../routes/auth"));
-app.use("/api/notes", require("../routes/notes"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 // app.use(express.static(path.join(__dirname, "../build")));
 // app.get("*", (req, res) => {
